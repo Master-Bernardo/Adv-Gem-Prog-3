0d4247569e4f248ea1da2fc0d657c0d9dece8632
Master-Bernardo <benito4747@live.com>
2018-06-21 18:34:48 +0200

    first prototype

---
+++ Adv GEm Prog 3/Assets/CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public float movementSpeed = 1f;
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontally = Input.GetAxis("Horizontal") * movementSpeed;
+        float moveVertically = Input.GetAxis("Vertical") * movementSpeed;
+
+        transform.position += new Vector3(moveHorizontally, 0f, moveVertically);
+    }
+}
