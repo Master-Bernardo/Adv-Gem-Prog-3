0126a2212ea57bf28f6ec9fa5f27acb6b4b87993
Master-Bernardo <benito4747@live.com>
2018-07-03 21:15:32 +0200

    added correct projectile motion
    updates every 2 seconds, so doesnt look smooth

--- Adv GEm Prog 3/Assets/Scripts/Weapons/Projectile.cs
+++ Adv GEm Prog 3/Assets/Scripts/Weapons/Projectile.cs
@@ -6,8 +6,19 @@ public class Projectile : MonoBehaviour {
 
     int damage;
     DamageType damageType;
+    [Tooltip("a bullet is a ball, but a bold or arrow are not")]
+    public bool ball = false;
 
-	public void setDamage(int damage, DamageType damageType)
+    public Rigidbody rb;
+
+    private void Start()
+    {
+            //for better performance, dont do this every frame
+            InvokeRepeating("AdjustRotation", 0f, 0.2f);
+  
+    }
+
+    public void setDamage(int damage, DamageType damageType)
     {
         this.damage = damage;
         this.damageType = damageType;
@@ -15,17 +26,24 @@ public class Projectile : MonoBehaviour {
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Unit")
-        {
-            collision.gameObject.GetComponent<UnitMovement>().GetDamage(damageType,damage);
+        if(collision.gameObject.tag != "Projectile") { 
+            if (collision.gameObject.tag == "Unit")
+            {
+                collision.gameObject.GetComponent<UnitMovement>().GetDamage(damageType,damage);
+            }
+
+            Destroy(gameObject.GetComponent<Rigidbody>()); //so our projectiles will stick - for now
+            if(!collision.gameObject.isStatic) transform.parent = collision.gameObject.transform; //cause some static objects like ground etc are scaled wrong
+            Destroy(gameObject,5);
         }
 
-        Destroy(gameObject.GetComponent<Rigidbody>()); //so our projectiles will stick - for now
-        if(!collision.gameObject.isStatic) transform.parent = collision.gameObject.transform; //cause some static objects like ground etc are scaled wrong
-        Destroy(gameObject,5);
+    }
 
-
-
-
+    void AdjustRotation()
+    {
+        if (!ball && rb != null)
+        {
+            transform.rotation = Quaternion.LookRotation(rb.velocity);
+        }
     }
 }
